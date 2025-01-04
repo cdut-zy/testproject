9f66abbecd74ff19e7022424cbf0e0ae146af22a
FiftyFive1998 <1274095017@qq.com>
2025-01-04 13:22:08 +0800

    测试代码提交

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    ……
+    overrides: [
+      {
+        files: ['src/views/**/*.vue'],
+        rules: {
+          'vue/multi-word-component-names': 0,
+        },
+      },
+    ]
+  }
+  
+  
